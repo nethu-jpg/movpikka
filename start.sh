8556a88acac6d773cccaae5e70ad2c2f1809fa4c
nethu-jpg <119031346+nethu-jpg@users.noreply.github.com>
2022-12-22 18:40:16 +0530

    4

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/MrDoCa/EvaMaria2.git /EvaMaria2
+  git clone https://github.com/nethu-jpg/movpikka.git /movpikka
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria2
+  git clone $UPSTREAM_REPO /movpikka
 fi
-cd /EvaMaria
+cd /movpikka
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
